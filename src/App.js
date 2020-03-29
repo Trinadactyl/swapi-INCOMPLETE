2f33c7cd848f9a6fec6869c2fd4113b25953da91
Trina <trinamceary@gmail.com>
2020-03-29 16:42:48 -0700

    initialized project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Welcome to my Starwars Api project!
+      Build using <a href="https://swapi.co/">This API</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
